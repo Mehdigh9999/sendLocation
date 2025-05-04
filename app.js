9fa75d0002d08d22e9cf32039bb459742a7891da
Mahdi <156158101+Mehdigh9999@users.noreply.github.com>
2025-05-03 20:27:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const routes = require('./routes/routes');
 
 app.set("view engine", "ejs");
